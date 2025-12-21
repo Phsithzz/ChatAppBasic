1ff34ba2ea034e5776833e1eb9bcb128f15b218e
Phsithzz <phoosith2548@gmail.com>
2025-12-21 20:05:58 +0700

    formate code

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,24 @@
-const {WebSocketServer} = require("ws")
-const wss = new WebSocketServer({port : 3001})
+const { WebSocketServer } = require("ws");
+const wss = new WebSocketServer({ port: 3001 });
 
-wss.on("connection",(ws)=>{
-    ws.on("message",(msg)=>{
-        try {
-            const data = JSON.parse(msg.toString())
-const messageWithTime = {
+wss.on("connection", (ws) => {
+  ws.on("message", (msg) => {
+    try {
+      const data = JSON.parse(msg.toString());
+      const messageWithTime = {
         ...data,
         time: new Date().toISOString(),
       };
-            wss.clients.forEach((client)=>{
-                if(client.readyState === ws.OPEN){
-                    client.send(JSON.stringify(messageWithTime))
-                }
-            })
-        } catch (err) {
-            console.log(err)
-            console.log("Invalid Json")
-            
+      wss.clients.forEach((client) => {
+        if (client.readyState === ws.OPEN) {
+          client.send(JSON.stringify(messageWithTime));
         }
-    })
-})
+      });
+    } catch (err) {
+      console.log(err);
+      console.log("Invalid Json");
+    }
+  });
+});
 
-console.log("WebSocket server running on ws://localhost:3001")
+console.log("WebSocket server running on ws://localhost:3001");
